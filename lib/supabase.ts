2464ab50af9c59f0d30e2c8b39913c7576d77d3a
naesunghan <fwshjh@naver.com>
2026-02-13 13:59:28 +0900

    vercel modify

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,11 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  db: {
-    schema: 'cpap_mask' // 커스텀 스키마 지정
-  }
-})
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 // 타입 정의
 export interface UserProfile {
